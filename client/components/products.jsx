4e2f1f42d533967cd29feb263d68fc4bf5511fbe
John Rodney <johnrodney@Johns-MacBook-Pro.local>
2017-08-01 03:27:47 -0500

    add a loader for products page and search bar, also adds media queries for products page

--- client/components/products.jsx
+++ client/components/products.jsx
@@ -7,6 +7,7 @@ export default class Products extends React.Component {
     super();
     this.state = {
       products: [],
+      query: '',
     };
   }
 
@@ -19,13 +20,23 @@ export default class Products extends React.Component {
   }
 
   render() {
+    const loading = this.state.products.length === 0;
+    const filteredProducts = this.state.products.filter(product => product.name.toLowerCase().indexOf(this.state.query.toLowerCase()) > -1);
+    console.log(this.state.query)
     return  (
       <div className="page">
         <div className="background"></div>
         <div className="products">
           <FixedHeader />
+          <div className='products-search'>
+            Search <input onChange={e => this.setState({ query: e.target.value })}/>
+          </div>
+          <div className='search-status'>
+            { this.state.query !== '' ? <div>Showing <span className='query-count'>{filteredProducts.length}</span> result(s) for <span className="query">{this.state.query}</span></div> : <div>Showing all <span className='query-count'>{filteredProducts.length}</span> results</div> }
+          </div>
+          { loading ? <div className="loader">Loading...</div> : <div></div>}
           {
-            this.state.products.map(product => (
+            filteredProducts.map(product => (
               <a href={product.link}>
                 <div className='a-product'>
                   <h1>{product.name}</h1>
